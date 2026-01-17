e02fd600894d887ec257b5b5ac3c82bcf9596a12
Priyanshu Maurya <159282128+XIZOAHUB@users.noreply.github.com>
2026-01-17 09:41:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ st.markdown("""
 
 # --- API SETUP (SECURE WAY) ---
 # Ye line check karegi ki key 'Secrets' mein hai ya nahi
-api_key = st.secrets.get("GEMINI_API_KEY")
+api_key = st.secrets.get("AIzaSyAz9A3w21sLRkUel1Sv5xNIs4M9GgDJ_tA")
 
 if not api_key:
     st.error("⚠️ API Key Missing! Setup instructions: Streamlit Cloud Settings > Secrets.")
